f2af5389a9f41edc2b3f468b74053de0d25c613a
Cornel <ncornel4444@gmail.com>
2022-10-25 21:48:01 +0300

    Update password.ts

--- src/utils/password.ts
+++ src/utils/password.ts
@@ -1,15 +1,15 @@
 const bcrypt = require('bcryptjs');
 
 export default class Password {
-    public async generateSalt(length: number = 10): Promise<string> {
-        return await bcrypt.genSalt(length);
+    async generateSalt(length: number = 10): Promise<string> {
+        return bcrypt.genSalt(length);
     }
 
-    public async generateHash(password: string, salt: string): Promise<string> {
-        return await bcrypt.hash(password, salt);
+    async generateHash(password: string, salt: string): Promise<string> {
+        return bcrypt.hash(password, salt);
     }
 
-    public async check(hash1: string, hash2: string): Promise<boolean> {
-        return await bcrypt.compare(hash1, hash2);
+    async check(hash1: string, hash2: string): Promise<boolean> {
+        return bcrypt.compare(hash1, hash2);
     }
 }
